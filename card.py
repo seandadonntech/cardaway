c1f169aec749e5252a37a6d65a43f9ed7cdf29ec
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-10-01 19:56:42 -0400

    Update card.py

--- card.py
+++ card.py
@@ -1,7 +1,8 @@
 from credit_card_checker import CreditCardChecker
-
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'A  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, Copyright (c) 2022, lilcryptotech All rights reserved. ')
 input = input("whats your credit card number?")
 card = CreditCardChecker(f'{input}').valid()
 print("notice if the number you type is true the card is correct otherwise its wrong or you didnt type something right!")
 print(card)
-
+# if you are going to be using this for recording or any public use please use the copyright thank you!

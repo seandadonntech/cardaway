610999d19c1a816d5c957c87452d84a749b5bb77
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-10-06 16:23:28 -0400

    Update card.py

--- card.py
+++ card.py
@@ -1,8 +1,23 @@
 from credit_card_checker import CreditCardChecker
-from colorama import Fore, Back, Style
+
+from colorama import Fore
 print(Fore.GREEN + 'A  program that genrates text into pyfiglet  made by seandadonntech , technio#7716, Copyright (c) 2022, lilcryptotech All rights reserved. ')
-input = input("whats your credit card number?")
+
+
+input = input("Enter a card number or type help:\n")
 card = CreditCardChecker(f'{input}').valid()
 print("notice if the number you type is true the card is correct otherwise its wrong or you didnt type something right!")
-print(card)
-# if you are going to be using this for recording or any public use please use the copyright thank you!
+print(card)# this function would is to return the value of the card as true or flase
+if input == "help":
+ print("A program to check if card is valid or not made by seandadonntech/lilcryptotech/technio#7716")
+if card == True: #if card is true the number is correct  or valid
+ print(f"{input} is a valid card number")
+elif card == False: # if card is
+ print(f" {input} is not a card number")
+
+else:
+ print(f" {input}  this number is not valid at all ")
+#add loop when i get home so program could restart
+#this is the github version pls don't forget to add the copy right
+#sooon program would see the card bank
+
